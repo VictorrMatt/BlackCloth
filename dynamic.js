7581a61bf8665ebe51f5ad0107992b09eecfcf16
Victor Mateus <98140122+VictorrMatt@users.noreply.github.com>
2022-08-08 16:17:12 -0300

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal("#myCarousel", {duration: 2000});
+sr.reveal("#content", {duration: 2000});
+sr.reveal("#about-us", {duration: 2000});
+sr.reveal("#contact-us", {duration: 2000});
