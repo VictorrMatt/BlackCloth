67ade9b09b7d3e2979f39dcdad191b7816640e33
Victor Mateus <98140122+VictorrMatt@users.noreply.github.com>
2022-08-08 19:21:54 -0300

    Soft-Changes

--- dynamic.js
+++ dynamic.js
@@ -1,6 +1,6 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal("#myCarousel", {duration: 2000});
-sr.reveal("#content", {duration: 2000});
-sr.reveal("#about-us", {duration: 2000});
-sr.reveal("#contact-us", {duration: 2000});
+sr.reveal("#myCarousel", {duration: 1500});
+sr.reveal("#content", {duration: 1500});
+sr.reveal("#about-us", {duration: 1500});
+sr.reveal("#contact-us", {duration: 1500});
